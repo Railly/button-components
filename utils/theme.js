3cbbe77732c71c8a31c46d0d9240a8cb7795c2a6
Railly Hugo <railly_2009@hotmail.com>
2021-07-03 13:38:54 -0500

    Add themes and states of buttons

---
+++ utils/theme.js
@@ -0,0 +1,18 @@
+const THEME = {
+  default: '#3F3F3F',
+  defaultBg: '#E0E0E0',
+  primary: '#2962FF',
+  secondary: '#455A64',
+  danger: '#D32F2F',
+  disabled: '#E0E0E0',
+  defaultHover: '#AEAEAE',
+  primaryHover: '#0039CB',
+  secondaryHover: '#1C313A',
+  dangerHover: '#9A0007',
+  fontDark: '#3F3F3F',
+  fontLight: '#FFF',
+  fontDisabled: '#9E9E9E',
+  white: '#FFF'
+}
+
+export default THEME
